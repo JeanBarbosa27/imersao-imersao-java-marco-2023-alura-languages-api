0114f0ac1b7c0dbd5798addaa05429b9821d37de
Jean Barbosa <jeanbarbosa9@gmail.com>
2023-03-31 11:42:04 +0100

    build: setup app with Spring Initializer

---
+++ src/main/java/com/jb9dev/languages/api/LanguagesApiApplication.java
@@ -0,0 +1,13 @@
+package com.jb9dev.languages.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguagesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguagesApiApplication.class, args);
+	}
+
+}
